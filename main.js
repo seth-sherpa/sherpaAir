ca5225d5ae9b800c6d16dda006841efb97788300
Seth Buechler <seth@FVFHKQ96Q6L4-Seth-Buechler.local>
2023-01-23 12:58:21 -0400

    improved flight menu

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ var ready = (callback) => {
 ready(() => {
     document.querySelector(".header").style.height = (window.innerHeight*0.75) + "px";
 })
+
+
+function getTodaysDate() {
+    var today = new Date().getFullYear()+'-'+("0"+(new Date().getMonth()+1)).slice(-2)+'-'+("0"+new Date().getDate()).slice(-2)
+    var nextWeek = new Date();
+    nextWeek.setDate(new Date().getDate() + 7);
+    var returnDate = nextWeek.getFullYear()+'-'+("0"+(nextWeek.getMonth()+1)).slice(-2)+'-'+("0"+nextWeek.getDate()).slice(-2)
+    document.getElementById("departDate").value = today;
+    document.getElementById("returnDate").value = returnDate;
+}
+
+
